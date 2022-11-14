6f58ec4075b934374291baa7fcddc97bdf30664f
ganett33 <65261313+ganett33@users.noreply.github.com>
2022-11-14 01:09:47 +0000

    add comment on 01 06b

--- 01_06/script.js
+++ 01_06/script.js
@@ -1,7 +1,9 @@
 const styleSheets = document.styleSheets;
+// assigned the ID from CSS
 const listButton = document.getElementById('list');
 const gridButton = document.getElementById('grid');
 
+// find specific style sheet in as document
 const styleSheetsArray = Array.from(styleSheets);
 const targetSheet = styleSheetsArray.find((styleSheet) => {
   if (styleSheet?.href) {
@@ -12,9 +14,13 @@ const targetSheet = styleSheetsArray.find((styleSheet) => {
   }
 });
 
+// disabled grid.css
 targetSheet.disabled = true;
+// add the class in the html
 listButton.classList.add('current');
 
+// when targetID is grid, remove current class form list button,
+// when not current class removed from grid button.
 const highlightSelected = (targetID) => {
   if (targetID === 'grid') {
     listButton.classList.remove('current');
@@ -25,11 +31,12 @@ const highlightSelected = (targetID) => {
   }
 };
 
+// callback function that when user click listview btn
 listButton.addEventListener('click', (e) => {
   highlightSelected(e.target.id);
   targetSheet.disabled = true;
 });
-
+// callback function that when user click gridview btn
 gridButton.addEventListener('click', (e) => {
   highlightSelected(e.target.id);
   targetSheet.disabled = false;
